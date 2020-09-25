9419579bfc086ffd26cc822fe973cb02ffcf0547
rviral <53877035+rviral001@users.noreply.github.com>
2020-09-25 17:30:29 -0500

    Add files via upload

---
+++ GameOfLife/src/LifeModel.java
@@ -0,0 +1,131 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.Timer;
+
+public class LifeModel implements ActionListener
+{
+
+	/*
+	 *  This is the Model component.
+	 */
+
+	private static int SIZE = 60;
+	private LifeCell[][] grid;
+	
+	LifeView myView;
+	Timer timer;
+
+	/** Construct a new model using a particular file */
+	public LifeModel(LifeView view, String fileName) throws IOException
+	{       
+		int r, c;
+		grid = new LifeCell[SIZE][SIZE];
+		for ( r = 0; r < SIZE; r++ )
+			for ( c = 0; c < SIZE; c++ )
+				grid[r][c] = new LifeCell();
+
+		if ( fileName == null ) //use random population
+		{                                           
+			for ( r = 0; r < SIZE; r++ )
+			{
+				for ( c = 0; c < SIZE; c++ )
+				{
+					if ( Math.random() > 0.85) //15% chance of a cell starting alive
+						grid[r][c].setAliveNow(true);
+				}
+			}
+		}
+		else
+		{                 
+			Scanner input = new Scanner(new File(fileName));
+			int numInitialCells = input.nextInt();
+			for (int count=0; count<numInitialCells; count++)
+			{
+				r = input.nextInt();
+				c = input.nextInt();
+				grid[r][c].setAliveNow(true);
+			}
+			input.close();
+		}
+
+		myView = view;
+		myView.updateView(grid);
+
+	}
+
+	/** Constructor a randomized model */
+	public LifeModel(LifeView view) throws IOException
+	{
+		this(view, null);
+	}
+
+	/** pause the simulation (the pause button in the GUI */
+	public void pause()
+	{
+		timer.stop();
+	}
+	
+	/** resume the simulation (the pause button in the GUI */
+	public void resume()
+	{
+		timer.restart();
+	}
+	
+	/** run the simulation (the pause button in the GUI */
+	public void run()
+	{
+		timer = new Timer(50, this);
+		timer.setCoalesce(true);
+		timer.start();
+	}
+
+	/** called each time timer fires */
+	public void actionPerformed(ActionEvent e)
+	{
+		oneGeneration();
+		myView.updateView(grid);
+	}
+
+	/** main logic method for updating the state of the grid / simulation */
+	private void oneGeneration(){
+		//student code
+		int sides = 0;
+		for (int r = 0; r < this.grid.length; r++){
+
+			for (int c = 0; c < this.grid[r].length; c++){
+				sides = 0;
+				//System.out.println(this.grid[r]);
+				for (int x = -1; x <= 1; x++){
+					for (int y = -1; y <= 1; y++){
+						if ((x != 0 || y != 0) && x + r >= 0 && x + r < this.grid.length && y + c >= 0 && y + c < this.grid[r].length){
+							//System.out.println(this.grid.length + ", " + this.grid[r].length);
+							if (this.grid[r + x][c + y].isAliveNow()){
+								sides++;
+							}
+						}
+					}
+				}
+
+				if (!this.grid[r][c].isAliveNow()){
+					if (sides == 3) {this.grid[r][c].setAliveNext(true);}
+				}
+				else if (this.grid[r][c].isAliveNow()){
+					if (sides>= 2 && sides <= 3){this.grid[r][c].setAliveNext(true);}
+					else{this.grid[r][c].setAliveNext(false);}
+				}
+
+			}
+		}
+
+		for (int r = 0; r < grid.length; r++){
+			for (int c = 0; c < grid[r].length; c++){
+				grid[r][c].setAliveNow(grid[r][c].isAliveNext());
+			}
+		}
+	}
+}
+
